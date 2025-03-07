cc843bd2eb0032f8b634304addc24075d5b14cc2
Christopher Riquelme <ccriquelmes@gmail.com>
2025-03-07 00:07:23 -0300

    Sección 5: data fetching

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,7 +1,12 @@
-export default function PokemonsPage() {
-  return (
-    <div>
-      <h1>Hello Pokemons Page</h1>
-    </div>
+const getPokemons = async ({ limit = 20, offset = 0 }) => {
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
   );
+  const data = await response.json();
+  return data;
+};
+
+export default async function PokemonsPage() {
+  const pokemons = await getPokemons({ limit: 20, offset: 0 });
+  return <div>{JSON.stringify(pokemons)}</div>;
 }
