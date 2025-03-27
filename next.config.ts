60bfe9cf04ee3c8f01dfac232e0bbbe8d09e95b8
Christopher Riquelme <ccriquelmes@gmail.com>
2025-03-27 15:37:10 -0300

    fix next.config.ts remotePatterns property

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,21 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["images.unsplash.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        port: "",
+        pathname:
+          "/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/photo-1542909168-82c3e7fdca5c",
+      },
+    ],
   },
 };
 
