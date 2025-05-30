52357164b4b1e54a54569d5cad63c8570a3d5edf
Christopher Riquelme <ccriquelmes@gmail.com>
2025-05-30 01:07:58 -0400

    Sección 5: Carga de información del pokemon por id y Metadata Dinamica

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,16 +1,40 @@
+import { Pokemon } from "@/pokemons";
+import { Metadata } from "next";
+
 interface PokemonPageProps {
   params: Promise<{
     id: string;
   }>;
 }
 
+export async function generateMetadata({
+  params,
+}: PokemonPageProps): Promise<Metadata> {
+  const { id, name } = await getPokemon((await params).id);
+
+  return {
+    title: `#${id} - ${name}`,
+    description: `Página del Pokémon ${name}`,
+  };
+}
+
+const getPokemon = async (id: string): Promise<Pokemon> => {
+  const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+    cache: "force-cache",
+  }).then((res) => res.json());
+
+  console.log("se cargo: ", pokemon.name);
+  return pokemon;
+};
+
 export default async function PokemonPage({ params }: PokemonPageProps) {
   const { id } = await params;
-  console.log(id);
+  const pokemon = await getPokemon(id);
 
   return (
     <div>
       <h1>Hello Pokemon {id} Page</h1>
+      <div>{pokemon.name}</div>
     </div>
   );
 }
